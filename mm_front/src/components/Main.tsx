681922b540baf5f22c36427dee87087892c1f471
RatedYK <terrence0101@yahoo.com>
2023-05-27 21:02:40 +0900

    fix: change endpoint to deployed back

--- mm_front/src/components/Main.tsx
+++ mm_front/src/components/Main.tsx
@@ -33,7 +33,7 @@ const Main = () => {
         const body = filterPrompt(userPrompt);
       
         try {
-          const response = await fetch('http://localhost:8000/movies', {
+          const response = await fetch('https://moviemood.onrender.com/movies', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json'
